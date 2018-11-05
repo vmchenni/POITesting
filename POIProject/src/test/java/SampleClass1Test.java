353c896a13512954976f871b06f0c026c44163b1
vmchenni <vishwanathchenni@gmail.com>
2018-11-05 18:12:21 +0800

    first commit

---
+++ POIProject/src/test/java/SampleClass1Test.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class SampleClass1Test {
+    @Test
+    public void fnMyFirstFunction(){
+        System.out.println("Inside fnMyFirstFunction");
+    }
+}
---
+++ POIProject/src/test/java/SampleClass2Test.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class SampleClass2Test {
+    @Test
+    public void fnMySecondFunction(){
+        System.out.println("InsideSecondFunction");
+    }
+}
